9e7a89605c8832751ec0670c067b8a0adffe85c0
Claudia <149967204+claudiahodoroga@users.noreply.github.com>
2025-01-05 21:04:04 +0100

    usuario implementacion

--- hatebuck/include/Enums.h
+++ hatebuck/include/Enums.h
@@ -2,25 +2,22 @@
 #define ENUMS_H
 
 
-class Enums
-{
-    public:
-        enum class TipoRelacion{
-            SALUDADO,
-            CONOCIDO,
-            AMIGO
-        };
-
-        enum class TipoContenido{
-            PALABRA,
-            SIMBOLO
-        };
-
-        enum class TipoPalabra{
-            REEMPLAZAR,
-            NOMOSTRAR
-        };
-
+enum class TipoRelacion{
+    SALUDADO,
+    CONOCIDO,
+    AMIGO
 };
+
+enum class TipoContenido{
+    PALABRA,
+    SIMBOLO
+};
+
+enum class TipoPalabra{
+    REEMPLAZAR,
+    NOMOSTRAR
+};
+
+
 
 #endif // ENUMS_H
--- hatebuck/src/Usuario.cpp
+++ hatebuck/src/Usuario.cpp
@@ -1,6 +1,30 @@
 #include "Usuario.h"
 
-Usuario::Usuario()
-{
-    //ctor
+bool Usuario::verificarPassword(const string& p) const{
+    return password == p;
+}
+
+const string& Usuario::obtNombre(){
+    return nombre;
+}
+
+void Usuario::publicarTexto(shared_ptr<IPublicacion> pub){
+    publicaciones.push_back(move(pub));
+}
+
+const vector<shared_ptr<IPublicacion>>& Usuario::obtPublicaciones() const{
+    return publicaciones;
+}
+
+void Usuario::establecerRelacion(const string& usuario, TipoRelacion tipo){
+    relaciones[usuario] = tipo;
+}
+
+bool Usuario::obtRelacion(const string& usuario, TipoRelacion& resultado) const{
+    auto it = relaciones.find(usuario);
+    if(it!=relaciones.end()){
+        resultado = it->second;
+        return true;
+    }
+    return false;
 }
